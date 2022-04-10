a1e1ffed8d87b59d99ae6f23892ad564912a39fa
Sydney Mabhena <sydneymabhena1@gmail.com>
2022-04-10 07:34:01 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * Entry point 
+ *
+ * Description: a program that prints lowercase alphabets
+ *
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 26; ++c)
+		putchar('a' +c);
+	putchar('\n');
+
+	return (0)
+}
