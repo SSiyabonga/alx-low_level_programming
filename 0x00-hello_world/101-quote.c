0919e19821262e23e3b3ef591ca671f13eb6c46d
Sydney Mabhena <sydneymabhena1@gmail.com>
2022-04-11 15:02:32 -0700

    8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: printing a quote without using printf or put.
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Kopar, 2015-10-19\n", 59);
+		return(1);
+}
