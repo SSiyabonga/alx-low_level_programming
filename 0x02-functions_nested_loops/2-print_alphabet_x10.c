640602650da2d006580445b5484f22596297c53e
Sydney Mabhena <sydneymabhena1@gmail.com>
2022-04-12 21:08:30 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 
 /**
  * Description - printing alphabets
- * print_alphabets_x10 - print alphabets 10 times
+ * print_alphabet_x10 - print alphabets 10 times
  * in lowercase
  */
 void print_alphabet_x10(void)
