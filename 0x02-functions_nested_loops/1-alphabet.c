365892bfae2b12f7112e6aea3c89e4b964beccd3
Sydney Mabhena <sydneymabhena1@gmail.com>
2022-04-12 20:53:33 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_alphabet - printing alphabets in lowercase followed by a new line
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'z; letter++)
+		_putchar(letter);
+	_putchar('\n');
+}
