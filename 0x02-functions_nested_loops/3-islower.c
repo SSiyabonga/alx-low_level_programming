6e3cc8a575ab3b52f112689a76a0070b68c06a07
Sydney Mabhena <sydneymabhena1@gmail.com>
2022-04-13 02:55:06 -0700

    3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _islower - Checks if character is lowercase.
+ * _islower - Checks for lowercase characters
  * @c: The character to be checked.
  * Return: 1 if c is lowercase, 0 otherwise
  */
