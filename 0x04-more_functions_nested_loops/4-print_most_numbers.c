07efa3ad365a57aa16f5b697689c742cd4f29c6a
Sydney Mabhena <sydneymabhena1@gmail.com>
2022-04-16 07:03:47 -0700

    4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -13,11 +13,10 @@ void print_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		if (i != 2 && i != 4)
-		{
+		if (i == 2 || i == 4)
+		i++;
 		_putchar(i + '0');
-
-	_putchar('\n');
-		}
 	}
+	_putchar('\n');
+
 }
