721e3c5bfded902be54e1557437db7daffbb760e
Sydney Mabhena <sydneymabhena1@gmail.com>
2022-04-16 06:46:50 -0700

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print the mumbers from 0 to 9
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+
+	for (i = 0; i < 10; i++)
+
+		_putchar(i + '0');
+	_putchar('\n');
+}
