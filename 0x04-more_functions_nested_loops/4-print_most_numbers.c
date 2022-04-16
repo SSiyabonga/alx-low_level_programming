1fbbbcf23a34e489e0644548d14ff48aeafd866a
Sydney Mabhena <sydneymabhena1@gmail.com>
2022-04-16 06:57:58 -0700

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_numbers - print the mumbers from 0 to 9
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+		_putchar(i + '0');
+
+	_putchar('\n');
+		}
+	}
+}
