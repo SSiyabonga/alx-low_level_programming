2d5062a5a4b95a5a63a9d840c111dfaab207c073
Sydney Mabhena <sydneymabhena1@gmail.com>
2022-04-20 15:34:28 -0700

    0

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 int _putchar(char c);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
-void _putchar(char *str);
+void _putcharn(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
