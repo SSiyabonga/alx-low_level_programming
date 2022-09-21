c9b2711f7125229f977d86f7963644ce3216fa77
Sydney <sydneymabhena1@gmail.com>
2022-09-21 09:54:14 +0200

    printing half of the srtring

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+* puts_half - a function that prints half of the string.
+* @str: sting variable
+*
+* Return: void.
+*/
+
+void puts_half(char *str)
+{
+	int i, e;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		continue;
+	}
+
+	if (i % 2 != 0)
+	{
+		e = (i / 2);
+		for (i = e; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		e = ((i + 1) / 2);
+		for (i = e; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+		_putchar('\n');
+	}
+return;
+}
+
