db21f0a7fec1b90422fc5b9687f09cbb6f1c473c
Sydney <sydneymabhena1@gmail.com>
2022-09-28 13:43:18 +0200

    puts with recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,12 +9,13 @@
 
 void _puts_recursion(char *s)
 {
-	int i = 0;
 
-	if (s[i] != '\0')
+	if (*s == '\0')
 	{
-		_putchar(s[i]);
-		_puts_recursion(s[(i + 1)]);
+		_putchar('\n');
+		return;
 	}
-	_putchar("\n");
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
