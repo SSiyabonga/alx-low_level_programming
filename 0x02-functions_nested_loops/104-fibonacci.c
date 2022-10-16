cc64ce559ac368c34dd955b7d40ca016d6c8c0b2
Sydney <sydneymabhena1@gmail.com>
2022-10-16 14:28:30 +0200

    fib98

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,14 +11,14 @@ int main(void)
 {
 	unsigned long n1 = 0, n2 = 1, n3, c = 0;
 
-	while (c < 99)
+	while (c < 97)
 	{
 		n3 = n2 + n1;
 		n1 = n2;
 		printf("%lu", n3);
 		n2 = n3;
 		c++;
-		if (c < 99)
+		if (c < 97)
 		{
 			printf(", ");
 		}
