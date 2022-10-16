c0eb65e491b5923bd832cdd298a8fd9944216c82
Sydney <sydneymabhena1@gmail.com>
2022-10-16 13:58:32 +0200

    sum

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - the function that prints the fibonacci sequence
+*
+* Return: 0.
+**/
+
+int main(void)
+{
+        unsigned long n1 = 0, n2 = 1, n3 = 0, sum = 0;
+
+        while (n3 < 4000000)
+        {
+                n3 = n2 + n1;
+                n1 = n2;
+                n2 = n3;
+		sum = sum + n3;
+        }
+	printf("%lu", sum);
+        printf("\n");
+        return (0);
+}
+
