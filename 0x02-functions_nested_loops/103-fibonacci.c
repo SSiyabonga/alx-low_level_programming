9e2d27ce9e0cd042d83b249c8afb66db433db1e5
Sydney <sydneymabhena1@gmail.com>
2022-10-16 14:04:56 +0200

    sum

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -11,12 +11,15 @@ int main(void)
 {
         unsigned long n1 = 0, n2 = 1, n3 = 0, sum = 0;
 
-        while (n3 < 4000000)
+        while (n3 < 4000001)
         {
                 n3 = n2 + n1;
                 n1 = n2;
                 n2 = n3;
-		sum = sum + n3;
+		if ((n3 % 2) == 0)
+		{
+			sum = sum + n3;
+		}
         }
 	printf("%lu", sum);
         printf("\n");
