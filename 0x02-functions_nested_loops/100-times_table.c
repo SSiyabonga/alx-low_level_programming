6accc771b1cb339becfbf4926f83e17356cb0231
Sydney <sydneymabhena1@gmail.com>
2022-10-16 14:59:25 +0200

    times9

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,61 +1,50 @@
-#include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
+#include"main.h"
 
 /**
-* print_times_table - The function that prints the n times table.
-* @n: The n of tables to be printed
 *
-* Return: nothing.
+* print_times_table - prints time table of n
+*
+* @n: takes number input
 **/
 
 void print_times_table(int n)
 {
-	int count, mult, prod, last;
+	int prod, mult, num;
 
-	if (n < 15)
+	if (n <= 15 && n >= 0)
 	{
-		for (count = 0; count <= n; count++)
+		for (num = 0; num <= n; ++num)
 		{
-			for (mult = 0; mult <= n; mult++)
-			{
-				prod = count * mult;
-				if (prod > 99)
-				{
-					last = prod % 100;
-					_putchar((prod / 100) + '0');
-					_putchar((last / 10) + '0');
-					_putchar((last % 10) + '0');
-					_putchar(',');
-					_putchar(' ');
-				}
-				else
-				{
-					if ((prod / 10) > 0)
-					{
-						_putchar((prod / 10) + '0');
-						_putchar((prod % 10) + '0');
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-					}
-					else
-					{
-						_putchar((prod % 10) + '0');
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(' ');
-					}
-				}
-			}
-			_putchar('\n');
+			_putchar(48);
 		}
-		return;
-	}
+		for (mult = 1; mult <= n; ++mult)
+		{
+			_putchar(',');
+			_putchar(' ');
 
-	else
-	{
-		return;
+			prod = num * mult;
+
+			if (prod <= 9)
+			{
+				_putchar(' ');
+			}
+			if (prod <= 99)
+			{
+				_putchar(' ');
+
+			}
+			if (prod >= 100)
+			{
+				_putchar((prod / 100) + 48);
+				_putchar((prod / 10) % 10 + 48);
+			}
+			else if (prod <= 99 && prod >= 10)
+			{
+				_putchar((prod / 10) + 48);
+				_putchar((prod % 10) + 48);
+			}
+		}
+			_putchar('\n');
 	}
+	return;
 }
