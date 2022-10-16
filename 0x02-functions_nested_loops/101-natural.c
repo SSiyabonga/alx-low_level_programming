5ec9762a58930a98de08574b260c4f90674e6b5c
Sydney <sydneymabhena1@gmail.com>
2022-10-16 11:43:52 +0200

    nat

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,18 +6,18 @@
 * Return: 0.
 **/
 
-int main (void)
+int main(void)
 {
-	int n = 0;
+	int n = 0, c = 0;
 
 	while (n < 1024)
 	{
 		if ((n % 3) == 0 || (n % 5) == 0)
 		{
-			printf("%d, ", n);
+			c = c + n;
 		}
 		n++;
 	}
-	printf("\n");
+	printf("%d\n", c);
 	return (0);
 }
