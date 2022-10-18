301e82f08450884569e47b43e5bbc4d2645d0fe8
Sydney <sydneymabhena1@gmail.com>
2022-10-18 09:15:40 +0200

    recursions

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,27 +1,35 @@
 #include "main.h"
+#include <stdio.h>
+
+int _sqrt(int n, int i);
 
 /**
-* _sqrt_recursion - the function which returns the square root of a number.
-* @n: the number whoes square root is to be calculated
-*
-* Return: the square root of n
-*/
-
+  * _sqrt_recursion - Returns the natural square root of a number
+  * @n: number to calculate the natural square root
+  *
+  * Return: the natural square root
+  */
 int _sqrt_recursion(int n)
 {
-	int x = n;
-
-	return (sqr_root(n, x));
+	return (_sqrt(n, 1));
 }
 
-
-int sqr_root(int n, int x)
+/**
+  * _sqrt - Calculates natural square root
+  * @n: number to calculate the square root
+  * @i: iterate number
+  *
+  * Return: the natural square root
+  */
+int _sqrt(int n, int i)
 {
-	if (n % (x - 1) != 0)
-	{
-		x--;
-		return (sqr_root(n, x));
-	}
+	int sqrt = i * i;
 
-	return (n / x);
+	if (sqrt > n)
+		return (-1);
+
+	if (sqrt == n)
+		return (i);
+
+	return (_sqrt(n, i + 1));
 }
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+* is_prime_number - a function that returns 1 if a n is a prime number
+*
+* @n: the number to be checked.
+* Return: 1 or zero
+**/
+
+int is_prime_number(int n)
+{
+	return (check_prime(n, 1));
+}
+
+/**
+  * check_prime - Check if number is prime
+  * @n: the number to be checked
+  * @i: the iteration times
+  *
+  * Return: 1 for prime or 0 composite
+  */
+int check_prime(int n, int i)
+{
+	if (n <= 1)
+		return (0);
+
+	if (n % i == 0 && i > 1)
+		return (0);
+
+	if ((n / i) < i)
+		return (1);
+
+	return (check_prime(n, i + 1));
+}
