eba6f3f6894ea69b0efe3ad8db375eb5917c0a62
Sydney <sydneymabhena1@gmail.com>
2022-10-24 13:54:59 +0200

    using predefined macros

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - a function that prints the name
+* of the file it is compiled on.
+* Return: Always 0.
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
