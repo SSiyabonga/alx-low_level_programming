f92491f28586c9c5392d78ebf9c415db18102bb0
Sydney <sydneymabhena1@gmail.com>
2022-10-25 23:42:15 +0200

    2 malloc tasks

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,9 +17,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i = 0, c, s;
 
 	if (s1 == NULL)
-                s1 = "";
-	 if (s2 == NULL)
-                s2 = "";
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	while (s1[i] != '\0')
 	{
