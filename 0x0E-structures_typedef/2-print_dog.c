0d42cf383e745b601d31e5b5f5eef832b64ae17d
Sydney <sydneymabhena1@gmail.com>
2022-10-30 05:00:18 +0200

    printing from a pointer to struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,11 +9,15 @@
 
 void print_dog(struct dog *d)
 {
-	d->name = name;
-	d->age = age;
-	d->owner=owner;
-
-	printf("Name: %s\n", name);
-	printf("Age: %f\n", age);
-	printf("Owner: %s\n", owner);
+	if (d == NULL)
+		return;
+	if (d->name != NULL)
+		printf("Name: %s\n", d->name);
+	else
+		printf("Name: (nil)\n");
+	printf("Age: %f\n", d->age);
+	if (d->owner != NULL)
+		printf("Owner: %s\n", d->owner);
+	else
+		printf("Owner: (nil)\n");
 }
